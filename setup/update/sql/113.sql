999f62b1a8a9b68852af6ebbf09dc41f56651a9c
landtax <109546080+landtax2@users.noreply.github.com>
2025-04-10 14:26:28 -0400

    fixed fkey

--- setup/update/sql/113.sql
+++ setup/update/sql/113.sql
@@ -11,6 +11,6 @@ CREATE TABLE public.network_file_fulltext (
 );
 CREATE INDEX network_file_fulltext_full_text_idx ON public.network_file_fulltext USING gin (to_tsvector('english'::regconfig, full_text));
 CREATE INDEX network_file_fulltext_network_file_id_idx ON public.network_file_fulltext USING btree (network_file_id);
-ALTER TABLE public.network_file_fulltext ADD CONSTRAINT network_file_fulltext_network_file_fk FOREIGN KEY (id) REFERENCES public.network_file(id) ON DELETE CASCADE;
+ALTER TABLE public.network_file_fulltext ADD CONSTRAINT network_file_fulltext_network_file_fk FOREIGN KEY (network_file_id) REFERENCES public.network_file(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 INSERT INTO public.config (setting, value, description, editable) VALUES ('STORE_FULLTEXT', '0', 'Used to determine if the full text of a network file should be stored in the database. 1 = store, 0 = do not store.', 1);
